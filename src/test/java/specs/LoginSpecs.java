3e9ef87e81c28821c7dc99efe9d9f80379a3e1c3
TMaksyutov <dadojkeee@mail.ru>
2022-12-03 15:08:16 +0300

    added Specs

---
+++ src/test/java/specs/LoginSpecs.java
@@ -0,0 +1,29 @@
+package specs;
+
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import static helpers.CustomApiListener.withCustomTemplates;
+import static io.restassured.filter.log.LogDetail.BODY;
+import static io.restassured.filter.log.LogDetail.STATUS;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.notNullValue;
+import static io.restassured.RestAssured.with;
+
+public class LoginSpecs {
+    public static RequestSpecification loginRequestSpec = with()
+            .filter(withCustomTemplates())
+            .baseUri("https://reqres.in")
+            .basePath("/api/login")
+            .log().all()
+            .contentType(JSON);
+
+    public static ResponseSpecification loginResponseSpec = new ResponseSpecBuilder()
+            .log(STATUS)
+            .log(BODY)
+            .expectStatusCode(200)
+            .expectBody("token", notNullValue())
+            .build();
+
+}
